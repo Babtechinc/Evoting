b9d7013cba4347bd1e98bc5e6f82b00b0abc80d8
adegbola.babs <adegbola.babs@GMAIL.COM>
2021-09-17 07:49:13 +0100

    Procfile edited

--- Evoting/settings.py
+++ Evoting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_$#gn1)ae+)28=df5#!hepmb71p)d56p7h*kc8(2_9z#0bs@2m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ogtan.herokuapp.com']
+ALLOWED_HOSTS = ['ogtan.herokuapp.com','127.0.0.1']
 
 
 # Application definition
